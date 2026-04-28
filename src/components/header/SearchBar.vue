d760cf322eb4734a94686eaf24754b71a79be997
pq87nkq <david.galindo@eduvaud.ch>
2026-04-28 14:01:21 +0200

    feat:"added search bar container binded to "term" reactive variable"

--- src/components/header/SearchBar.vue
+++ src/components/header/SearchBar.vue
@@ -1,9 +1,17 @@
 <script setup>
-
 </script>
 
 <template>
-
+<div>
+  <form @submit.prevent="onSearch" class="search-container">
+    <input
+      type="text"
+      v-model="term"
+      placeholder="Search music..."
+      class="search-input"
+    >
+  </form>
+</div>
 </template>
 
 <style scoped>
