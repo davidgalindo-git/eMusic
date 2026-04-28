a8e3a6334ded3df0405e67a6e4c6e15f4e412d77
pq87nkq <david.galindo@eduvaud.ch>
2026-04-28 14:07:29 +0200

    feat: connect SearchBar to iTunes API composable (#8)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
-</script>
+import { useITunes } from "./api/useITunes.js";
+import SearchBar from "./components/header/SearchBar.vue";
 
+const { songs, searchSongs, loading} = useITunes();
+
+const handleSearch = (term) => {
+  searchSongs(term)
+}
+</script>
 <template>
+  <SearchBar @search="handleSearch" />
+  <div class="songs">
+    {{ songs }}
+  </div>
 </template>
