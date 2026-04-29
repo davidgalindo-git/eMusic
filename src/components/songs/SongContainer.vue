9dfaa4dc6221cccae0b7008b54b634c61d3dbfa6
davidgalindo-git <>
2026-04-29 10:33:28 +0200

    refactor:"added song store call and SongCard props in SongContainer"

--- src/components/songs/SongContainer.vue
+++ src/components/songs/SongContainer.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { useSongStore } from "../../store/useSongStore.js";
 import SongCard from "./SongCard.vue";
+
+const songStore = useSongStore()
 </script>
 
 <template>
   <v-container>
-    <SongCard />
+    <v-row>
+      <v-col
+        v-for="song in songStore.songs"
+        :key="song.trackId"
+        cols="12" sm="6" md="4" lg="3"
+      >
+        <SongCard :song="song"/>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
