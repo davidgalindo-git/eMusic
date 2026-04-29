df0d4947043e5a10770267440d3bab365e7da8d9
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-04-29 09:32:02 +0200

    feat:"added card container template from vuetifyjs.com (#20)"

--- src/components/songs/SongCard.vue
+++ src/components/songs/SongCard.vue
@@ -3,7 +3,29 @@
 </script>
 
 <template>
+  <v-row density="comfortable">
+    <v-col
+        v-for="(variant, i) in variants"
+        :key="i"
+        cols="12"
+        md="4"
+    >
+      <v-card
+          :variant="variant"
+          class="mx-auto"
+          color="surface-variant"
+          max-width="344"
+          subtitle="Greyhound divisely hello coldly fonwderfully"
+          title="Headline"
+      >
+        <template v-slot:actions>
+          <v-btn text="Button"></v-btn>
+        </template>
+      </v-card>
 
+      <div class="text-center text-body-small">{{ variant }}</div>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
