0db348ba624bee88500ae3a1539f19fc9aa1758a
davidgalindo-git <>
2026-04-29 11:23:15 +0200

    refactor: rm button on card

--- src/components/songs/SongCard.vue
+++ src/components/songs/SongCard.vue
@@ -10,19 +10,16 @@ const variants = ['elevated', 'flat', 'tonal', 'outlined'];
 </script>
 
 <template v-slot:actions>
-  <v-btn>
-    <v-card
-        :variant="variants[0]"
-        class="mx-auto"
-        color="surface-variant"
-        max-width="344"
-        :title="song.trackName"
-        :subtitle="song.artistName"
-    >
-    </v-card>
-  </v-btn>
+  <v-card
+      :variant="variants[0]"
+      class="mx-auto"
+      color="surface-variant"
+      max-width="344"
+      :title="song.trackName"
+      :subtitle="song.artistName"
+  >
+  </v-card>
 </template>
 
 <style scoped>
-
 </style>
