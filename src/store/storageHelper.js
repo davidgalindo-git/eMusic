be799cab5cfb19339868ac56e858f2bedd1f6a2d
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-04-30 10:55:35 +0200

    refactor: added try catch to prevent invalid JSON error

--- src/store/storageHelper.js
+++ src/store/storageHelper.js
@@ -5,11 +5,20 @@
 const STORAGE_KEY = 'emusic_data';
 
 // Récupère l'objet global du projet
-const getRawStore = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
+const getRawStore = () => {
+    try {
+        const data = localStorage.getItem(STORAGE_KEY);
+        return data ? JSON.parse(data) : {};
+    } catch (error) {
+        // En cas de JSON invalide, retourner un objet vide
+        console.error("LocalStorage corrompu, réinitialisation des données temporaires.");
+        return {};
+    }
+};
 
 /**
  * Sauvegarde une collection spécifique
- * @param {string} collectionName - Nom de la clé (ex: 'à la une')
+ * @param {string} collectionName - Nom de la clé (ex: 'search_results' ou 'à la une')
  * @param {Array} data - Tableau de chansons mappées
  */
 export const saveCollection = (collectionName, data) => {
