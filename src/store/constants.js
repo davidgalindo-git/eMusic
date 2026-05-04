08d02354ca4628f11fcde9eb29d834b5ffc636a8
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-05-04 14:31:47 +0200

    feat: added test default collection to constants

---
+++ src/store/constants.js
@@ -0,0 +1,16 @@
+export const DEFAULT_COLLECTION = [
+    {
+        trackId: 101,
+        trackName: 'Default Track 1',
+        artistName: 'App Featured',
+        previewUrl: '',
+        artworkUrl: ''
+    },
+    {
+        trackId: 102,
+        trackName: 'Default Track 2',
+        artistName: 'App Featured',
+        previewUrl: '',
+        artworkUrl: ''
+    }
+];
