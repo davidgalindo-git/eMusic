0f2a2c33eb5191b107ec73b254949a476b2aac51
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-05-06 11:55:30 +0200

    feat: added playlist card with name and total songs

--- src/components/playlist/PlaylistCard.vue
+++ src/components/playlist/PlaylistCard.vue
@@ -14,9 +14,17 @@ const variants = CARD_VARIANTS;
 </script>
 
 <template>
-<v-card>
-
-</v-card>
+  <v-card
+    :variant="variants"
+    class="playlist-card mx-auto"
+    color="surface-variant"
+    max-width="100"
+    height="30"
+    @click="emit('select', playlist.id)"
+  >
+    <v-card-title>{{ playlist.name }}</v-card-title>
+    <v-card-text>{{ playlist.songs.length }} songs</v-card-text>
+  </v-card>
 </template>
 
 <style scoped>
