0e1dbb0265c3f992c5f5da15ae7b15092cbd11ef
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-05-06 11:29:08 +0200

    feat: added basic styling for playlist side menu

--- src/components/playlist/PlaylistSideMenu.vue
+++ src/components/playlist/PlaylistSideMenu.vue
@@ -12,5 +12,40 @@
 </template>
 
 <style scoped>
+.playlist-menu {
+  /* Positioning and Size */
+  width: 250px;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  /* Layout */
+  display: flex;
+  flex-direction: column;
+
+  /* Visuals */
+  background-color: #121212;
+  color: white;
+  border-right: 1px solid #282828;
+}
+
+.menu-header {
+  padding: 24px;
+  border-bottom: 1px solid #282828;
+}
+
+.menu-header h2 {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: white;
+}
+
+.playlist-container {
+  flex: 1;
+  overflow-y: auto;
+  padding: 16px 0;
+}
 
 </style>
