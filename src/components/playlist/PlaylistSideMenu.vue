fd405c774c6658d51f43217cb0dc5aa8a187e71c
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-05-06 11:28:48 +0200

    feat: added playlist side menu template using PlaylistContainer

--- src/components/playlist/PlaylistSideMenu.vue
+++ src/components/playlist/PlaylistSideMenu.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-
+  <aside class="playlist-menu">
+    <div class="menu-header">
+      <h2>Playlists</h2>
+    </div>
+    <PlaylistContainer class="playlist-container"/>
+  </aside>
 </template>
 
 <style scoped>
