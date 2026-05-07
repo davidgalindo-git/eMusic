b534e191dbbbb7eda87502f04577d7989b56e39b
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-05-07 08:37:25 +0200

    feat: added PlaylistSongCard based on SongCard structure

--- src/components/playlist/page/PlaylistSongCard.vue
+++ src/components/playlist/page/PlaylistSongCard.vue
@@ -1,11 +1,50 @@
 <script setup>
+import {CARD_VARIANTS} from "../../../store/constants.js";
+import AddToPlaylistButton from "./../../songs/AddToPlaylistButton.vue";
 
+const props = defineProps({
+  song: {
+    type: Object,
+    required: true
+  }
+});
+
+const emit = defineEmits(["toggle-play"]);
+
+const variants = CARD_VARIANTS;
 </script>
 
 <template>
-
+  <v-card
+      :variant="variants[0]"
+      class="song-card mx-auto"
+      color="surface-variant"
+      max-width="604"
+      height="170"
+      @click="emit('toggle-play', song)"
+  >
+    <v-img
+        :src="song.artworkUrl"
+        cover
+        class="flex-shrink-0 flex-grow-0"
+        style="flex-basis: 170px;"
+    ></v-img>
+    <div class="d-flex flex-column justify-center flex-grow-1 overflow-hidden">
+      <v-card-item
+          :title="song.trackName"
+          :subtitle="song.artistName"
+      >
+      </v-card-item>
+    </div>
+    <AddToPlaylistButton :song="song" />
+  </v-card>
 </template>
 
 <style scoped>
-
+.song-card{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  overflow: hidden;
+}
 </style>
